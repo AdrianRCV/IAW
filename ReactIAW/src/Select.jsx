dbceed3ac95e718a6a32c758238fbcc472e5813d
AdrianRCV <adriancamposvargas4@gmail.com>
2024-11-06 16:54:26 +0100

    20241106(2)

--- ReactIAW/src/Select.jsx
+++ ReactIAW/src/Select.jsx
@@ -4,48 +4,21 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import json2 from './heroes.json'; // Asegúrate de que la ruta sea correcta
+import json2 from './heroes.json'; 
 
 export default function HeroSelect() {
   const [hero, setHero] = React.useState('');
-  const [publisher, setPublisher] = React.useState(''); // Estado para el publisher seleccionado
 
-  // Función que maneja el cambio de selección de héroe
+  const marvelHeroes = json2.filter(heroOption => heroOption.publisher.trim().toLowerCase() === "marvel comics");
+
   const handleHeroChange = (event) => {
     setHero(event.target.value);
   };
 
-  // Función que maneja el cambio de publisher
-  const handlePublisherChange = (event) => {
-    setPublisher(event.target.value);
-  };
-
-  // Filtrar héroes por publisher
-  const filteredHeroes = publisher
-    ? json2.filter((heroOption) => heroOption.publisher === publisher)
-    : json2;  // Si no hay publisher seleccionado, no filtra y muestra todos los héroes.
-
-  console.log(filteredHeroes); // Agrega esta línea para verificar los héroes filtrados en la consola
-
   return (
-    <Box sx={{ minWidth: 120 }}>
+    <Box sx={{ minWidth: 252 }}>
       <FormControl fullWidth>
-        <InputLabel id="publisher-select-label">Select Publisher</InputLabel>
-        <Select
-          labelId="publisher-select-label"
-          id="publisher-select"
-          value={publisher}
-          label="Select Publisher"
-          onChange={handlePublisherChange}
-        >
-          <MenuItem value="">All</MenuItem>   {/* Opción para no filtrar */}
-          <MenuItem value="Marvel">Marvel</MenuItem>
-          <MenuItem value="DC Comics">DC Comics</MenuItem>
-        </Select>
-      </FormControl>
-
-      <FormControl fullWidth>
-        <InputLabel id="hero-select-label">Select Hero</InputLabel>
+        <InputLabel id="hero-select-label">Hero</InputLabel>
         <Select
           labelId="hero-select-label"
           id="hero-select"
@@ -53,7 +26,8 @@ export default function HeroSelect() {
           label="Select Hero"
           onChange={handleHeroChange}
         >
-          {filteredHeroes && filteredHeroes.map((heroOption) => (
+          {/* Solo renderiza las opciones si hay héroes filtrados */}
+          {marvelHeroes.map((heroOption) => (
             <MenuItem key={heroOption.superhero} value={heroOption.superhero}>
               {heroOption.superhero} ({heroOption.publisher})
             </MenuItem>
--- ReactIAW/src/paginaWeb/Cards.jsx
+++ ReactIAW/src/paginaWeb/Cards.jsx
@@ -1,38 +1,42 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import style from './cards.module.css';
 
 function Tarjeta() {
   return (
     <>
-      <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="/paisaje.jpeg" />
+      <Card style={{ width: '18rem', height: '150px'}} className={style.imgcards}>
+          <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/250px-React.svg.png" />
           <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>React</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+          React es una biblioteca de JavaScript de código abierto utilizada 
+          para construir interfaces de usuario, especialmente para 
+          aplicaciones web de una sola página.
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
           </Card.Body>
       </Card>
-      <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="/paisaje.jpeg" />
+      <Card style={{ width: '18rem' }} className={style.imgcards}>
+          <Card.Img variant="top" src="https://www.digitality.es/img-articulos/ampliadas/que-es-nextjs-y-para-que-sirve-1-1697560678.jpg" />
           <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Next</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+           Next.js es un framework de React que facilita la creación de aplicaciones
+           web modernas y optimizadas, proporcionando una serie de características
+           y herramientas adicionales.
+          </Card.Text> 
           <Button variant="primary">Go somewhere</Button>
           </Card.Body>
       </Card>
-      <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="/paisaje.jpeg" />
+      <Card style={{ width: '18rem' }} className={style.imgcards}>
+          <Card.Img variant="top" src="https://miro.medium.com/v2/resize:fit:1400/1*s9kgU8F1eB7Tzs7sG0YhBg.jpeg" />
           <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Nest</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+          NestJS es un framework de desarrollo backend para Node.js que utiliza 
+          TypeScript por defecto y se basa en una arquitectura de modulos y 
+          controladores similar a la de frameworks como Angular.
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
           </Card.Body>
