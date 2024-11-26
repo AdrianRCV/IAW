5ec03827d507eef8ada1bb958a9b22ec5fc006d3
AdrianRai <adriancamposvargas4@gmail.com>
2024-11-26 09:30:09 +0100

    20241126

--- nextasir/app/pokemonNext2/page.jsx
+++ nextasir/app/pokemonNext2/page.jsx
@@ -1,4 +1,3 @@
-
 import { Suspense } from "react"
 import { FetchPokemon } from "@/componentes/FetchPokemon"
 import ErrorBoundary from "./error"
@@ -6,14 +5,9 @@ import ErrorBoundary from "./error"
 const Page = ({}) => {
   return (
     <ErrorBoundary>
-        <Suspense fallback={<h2>Cargando....</h2>}><FetchPokemon/></Suspense>
+        <Suspense fallback={<h2>Cargando Pokemon 2....</h2>}><FetchPokemon/></Suspense>
     </ErrorBoundary>
   )
 }
-
-export const metadata = {
-    title: "Página leyendo Pokemon NEXT",
-    description: "Accediendo a POKAPI",
-  };
 
 export default Page
